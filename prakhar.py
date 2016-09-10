e4b1b4effd621d3ee8f838133e4e2d37a3c3c226
2016JTM2080 <prakhararya25@gmail.com>
2016-09-10 16:03:38 +0530

    Add files via upload

--- prakhar.py
+++ prakhar.py
@@ -1,1 +1,37 @@
-print " Hello world "
+
+str1 =" Hello world "
+print str1
+print str1[1] 
+if True:
+	print " true "
+	print " false "
+import sys
+print "Number of arguments :", len(sys.argv)
+print str(sys.argv)
+v="prakhar"
+print " my name is ", v
+
+a=b=c=1
+if (a is b) :
+	print 'a nd b have same id'
+var1=0
+print var1
+list = [ 1, 2 ,3 , 'prakhar ']
+c = 1
+if (c in list) :
+	print "c is in list"
+print c
+print list
+tinylist = [ 1, 3, 'dheeraj' ]
+print list + tinylist
+print list[2:]
+tinydict ={'name':'john','code':6734,'dept':'sales'}
+# Prints value for 2 key
+print tinydict          
+# Prints complete dictionary
+print tinydict.keys() # parenthesis important
+# Prints all the keys
+print tinydict.values() # paranthesis important
+# Prints all the value
+
+raw_input("\n\nPress the enter key to exit.")
